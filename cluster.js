17fa0d04c8e130a29d392691374e6da54aa90ceb
Ergun Coruh <ergun.coruh@gmail.com>
2017-12-13 21:48:58 +1100

    cluster prototype

---
+++ cluster.js
@@ -0,0 +1,37 @@
+const cluster = require('cluster');
+
+function waitms(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve => resolve(), ms, resolve);
+  });
+}
+
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  const worker = cluster.fork();
+
+  waitms(10000)
+    .then(() => {
+      worker.disconnect();
+      timeout = setTimeout(() => {
+        worker.kill();
+      }, 2000);
+    });
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+
+} else {
+
+  function tick() {
+    console.log('.');
+  }
+
+  console.log(`Worker ${cluster.worker.id} is running`);
+  setInterval(() => tick(), 1000);
+
+}
